6b3c4f4c1cc180f2f7fe6df11d789fa66157aaf3
anamerasul <anamerasul@gmail.com>
2022-02-14 16:45:48 +0600

    case convert

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+const anthem = 'Amar Sonar Bangla Ami Tumay valobasi';
+
+const search ='valo';
+
+
+const userInput='blackPink';
+const  savedUserName='blackpink';
+
+if (userInput.toLowerCase()==savedUserName.toLowerCase()){
+        console.log('user exit')
+}
+
+// toLowerCase() and toUpperCase()
