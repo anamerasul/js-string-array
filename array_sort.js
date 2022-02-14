87aba4f4deae6c20be3e80f2e383a679ff60a7c0
anamerasul <anamerasul@gmail.com>
2022-02-14 18:44:44 +0600

    Have fun with sort and reverse a javascript array

---
+++ array_sort.js
@@ -0,0 +1,28 @@
+const numbers=[6,4,7,1,3,9,2,5];
+
+const sortedNumbers=numbers.sort();
+
+console.log(sortedNumbers);
+
+const friends=['kabil','josim','razzak','anwor','dipjol','badsha'];
+
+const sortedFriends=friends.sort();
+
+console.log(sortedFriends);
+
+const reversedFriends=friends.reverse();
+
+console.log(reversedFriends);
+
+
+//nurbers sorting fun
+
+const bigNumbers=[66,58,81,92,9,6,37,13,2];
+
+const sortedBigNumbers=bigNumbers.sort(
+        function(a,b){
+                return a - b;
+        }
+);
+
+console.log(sortedBigNumbers)
