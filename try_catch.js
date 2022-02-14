4787b501e7a2acacf54994579f0cc3446c61d5bb
anamerasul <anamerasul@gmail.com>
2022-02-14 19:41:04 +0600

    Different types of Error object and explore Try-catch

---
+++ try_catch.js
@@ -0,0 +1,25 @@
+const myname='ami';
+// myname='me'
+// name='tumi';
+// console.log(name);
+
+// for(let i=0; i<10; i++){
+
+// }
+
+// const student=undefined;
+
+// console.log(student());
+// name='tumi';
+// throw 'err'
+try{
+        myname='tumi';
+}
+
+
+catch(error){
+console.log( "my err",error);
+}
+
+console.log(myname)
+console.log(100)
